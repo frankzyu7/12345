41727952464b6e9047c0c919f2ecec66af3eaae1
Sihan Liu <sihanl5@student.unimelb.edu.au>
2018-09-25 10:36:24 +1000

    add coordinate label

--- Crossword.java
+++ Crossword.java
@@ -1,3 +1,4 @@
+
 //12345
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
@@ -18,127 +19,116 @@ import javax.swing.*;
 
 public class Crossword {
 
-	private JFrame f;
-	private static char[][] charSet;
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Crossword window = new Crossword();
-					window.f.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-				
-				/*
-				charSet = new char[20][20];
-				for (int i=0;i<20;i++) {
-					for (int j = 0; j<20;j++) {
-						charSet[i][j]=
-					}
-				}
-				*/
-			}
-		});
-	}
+    private JFrame f;
+    private static char[][] charSet;
 
-	/**
-	 * Create the application.
-	 */
-	public Crossword() {
-		initialize();
-	}
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    Crossword window = new Crossword();
+                    window.f.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		f = new JFrame();
-		f.setBounds(100, 100, 800, 800);
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                /*
+                 * charSet = new char[20][20]; for (int i=0;i<20;i++) { for (int j = 0;
+                 * j<20;j++) { charSet[i][j]= } }
+                 */
+            }
+        });
+    }
+
+    /**
+     * Create the application.
+     */
+    public Crossword() {
+        initialize();
+    }
+
+    /**
+     * Initialize the contents of the frame.
+     */
+    private void initialize() {
+        f = new JFrame();
+        f.setBounds(100, 100, 800, 800);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.getContentPane().setLayout(new BorderLayout());
 
         JPanel container = new JPanel(new FlowLayout());
         final CrosswordPanel panel = new CrosswordPanel();
         container.add(panel);
         f.getContentPane().add(container, BorderLayout.CENTER);
-        
+
         generate(panel);
-  
-    
-        
 
         f.setSize(800, 800);
         f.setLocationRelativeTo(null);
         f.setVisible(true);
 
+    }
 
-	}
-	
-	private static void generate(CrosswordPanel panel)
-	{
-		int w = 20;
-		int h = 20;
-		char crossword[][] = new char[w][h];
-		for (int y=0; y<h; y++)
-		{
-			for (int x=0; x<w; x++)
-			{
-					crossword[x][y] = '\u0020'; // null character
-				
-			}
-		}
+    private static void generate(CrosswordPanel panel) {
+        int w = 20;
+        int h = 20;
+        char crossword[][] = new char[w][h];
+        for (int y = 0; y < h; y++) {
+            for (int x = 0; x < w; x++) {
+                crossword[x][y] = '\u0020'; // null character
 
-		panel.setCrossword(crossword);
-	}
+            }
+        }
 
-	class CrosswordPanel extends JPanel
-	{
-		private JTextField textFields[][];
+        panel.setCrossword(crossword);
+    }
 
-		void setCrossword(char array[][])
-		{
-			removeAll();
-			int w = array.length;
-			int h = array[0].length;
-			setLayout(new GridLayout(w, h));
-			textFields = new JTextField[w][h];
-			
+    class CrosswordPanel extends JPanel {
+        private JTextField textFields[][];
+        private JLabel label[][];
 
-			for (int y=0; y<h; y++)
-			{
-				for (int x=0; x<w; x++)
-				{
-					char c = array[x][y];
-					if (c != 0)
-					{
-						textFields[x][y] = new JTextField(String.valueOf(c));
-						textFields[x][y].setFont(textFields[x][y].getFont().deriveFont(20.0f));
-				        textFields[x][y].setColumns(1); // the widths of the textfileds
+        void setCrossword(char array[][]) {
+            removeAll();
+            int w = array.length;
+            int h = array[0].length;
+            setLayout(new GridLayout(w + 1, h + 1));
+            textFields = new JTextField[w][h];
+            label = new JLabel[w + 1][h + 1];
+            for (int x = -1; x < w; x++) {
+                // label[x][y] = new JLabel(String.valueOf(x+1));
+                add(new JLabel(String.valueOf(x + 1)));
+            }
+            for (int y = 0; y < h; y++) {
+                for (int x = 0; x < w; x++) {
+                    char c = array[x][y];
+                    if (x == 0) {
+                        add(new JLabel(String.valueOf(y + 1)));
+                    }
+                    if (c != 0) {
+                        textFields[x][y] = new JTextField(String.valueOf(c));
+                        textFields[x][y].setFont(textFields[x][y].getFont().deriveFont(20.0f));
+                        textFields[x][y].setColumns(1); // the widths of the textfileds
 
-						add(textFields[x][y]);
-					}
-					else
-					{
-						add(new JLabel());
-					}
-				}
-			}
-			getParent().validate();
-			repaint();
-		}
+                        add(textFields[x][y]);
+                    } else {
+                        add(new JLabel());
+                    }
+                }
+            }
+            getParent().validate();
+            repaint();
+        }
 
-		char[][] getCrossword()
-		{
-			int w = textFields.length;
-			int h = textFields[0].length;
-			char crossword[][] = new char[w][h];
-			
-			return crossword;
-		}
+        char[][] getCrossword() {
+            int w = textFields.length;
+            int h = textFields[0].length;
+            char crossword[][] = new char[w][h];
 
-	}
+            return crossword;
+        }
+
+    }
 }
